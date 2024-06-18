14ded55cb320af81958c4a2da334f114d0e789a3
grokpaperscissors <shreyakumar.tech9@gmail.com>
2024-06-18 16:34:54 +0530

    code: website hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -8,12 +8,13 @@ const Hero = () => {
     <HeroContainer>
         <HeroBG>
             <VideoBG src={Video} type="video/mp4" autoPlay loop muted playsInline/>
+            <Overlay/>
         </HeroBG>
         <HeroContent>
             <HeroItems>
                 <HeroH1>Music for Everyone</HeroH1>
-                <HeroP>Out of this world</HeroP>
-                <Button>Donate Now</Button>
+                <HeroP>Inclusive music</HeroP>
+                <Button primary = "true" big="true" round="true" to="/trips">Donate Now</Button>
             </HeroItems>
         </HeroContent>
     </HeroContainer>
@@ -32,6 +33,28 @@ const HeroContainer = styled.div`
     padding: 0 1rem;
     position: relative;
     margin-top: -80px;
+
+    // overlay
+
+    // :before {
+    //     content: "",
+    //     position: absolute;
+    //     top: 0;
+    //     bottom: 0;
+    //     right: 0;
+    //     left: 0;
+    //     z-index: 2;
+    //     background: linear-gradient(
+    //                     180deg, 
+    //                     rgba(0,0,0,0.2) 0%, 
+    //                     rgba(0,0,0,0.6) 100%
+    //                     ),
+    //                 linear-gradient(
+    //                 180deg, 
+    //                 rgba(0,0,0,0.2) 0%, 
+    //                 transparent 100%);
+
+    // }
 `
 
 const HeroBG = styled.div`
@@ -45,12 +68,21 @@ const HeroBG = styled.div`
     overflow: hidden;
 `
 
-
 const VideoBG = styled.video`
     width: 100%;
     height: 100%;
     -o-object-fit: cover;
     object-fit: cover;
+`
+
+const Overlay = styled.div`
+  position: absolute;  // Same positioning to cover the video
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.3);  // Semi-transparent black
+  z-index: 1;  // Ensures it's above the video
 `
 
 const HeroContent = styled.div`
@@ -74,6 +106,18 @@ const HeroItems = styled.div`
     font-weight: bold;
 `
 
-const HeroH1 = styled.h1``
+const HeroH1 = styled.h1`
+    // resize the font based on screen size
+    font-size: clamp(1.5rem, 6vw, 4rem);
+    margin-bottom: 1.5rem;
+    letter-spacing: 3px;
+    font-weight: bold;
+    padding: 0 1rem;
+`
 
-const HeroP = styled.p``
+const HeroP = styled.p`
+    font-size: clamp(1rem, 3vw, 3.5rem);
+    margin-bottom: 2rem;
+    font-weight: 400;
+`
+
