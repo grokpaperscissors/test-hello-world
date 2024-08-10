884f886f611181fc15904d6173d025e16beba008
grokpaperscissors <shreyakumar.tech9@gmail.com>
2024-08-10 19:08:15 +0530

    Update menuData.js

--- src/data/menuData.js
+++ src/data/menuData.js
@@ -1,6 +1,6 @@
 export const menuData = [
     {title: "About",link: "/about"},
-    {title: "Trips",link: "/trips"},
+    {title: "Programs",link: "/programs"},
     {title: "Stats",link: "/stats"},
     {title: "Contact",link: "/contact"},
 ]
